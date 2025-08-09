c4bcb4d45a07bc92feea9ae86b94380c0c780a6d
Krystian Kurlej <235661@student.uek.krakow.pl>
2025-08-09 20:22:01 +0200

    Enhance layout and project form
    - Updated layout.latte to include Bootstrap CSS and jQuery for improved styling and functionality.
    - Added a main.js script reference at the end of the body for custom JavaScript.
    - Modified projects.latte to change the form ID to 'createProjectForm', added a POST method, and included a hidden input for the action to streamline project creation.

---
+++ public/js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    $('form.form').attr("novalidate", true).on("submit", function (event) {
+        const method = $(this).attr("method");
+        const form = $(this);
+        const url = $(this).attr("action");
+        const successModal = $(this).data("success-modal") || false;
+        const errorModal = $(this).data("error-modal") || false;
+
+        sendForm(method, form, url, event, successModal, errorModal);
+    });
+});
+
+function sendForm(method, form, endpoint, event, successModal, errorModal){
+    event.preventDefault();
+
+    form.find(':required').each(function() {
+        $(this).removeClass('is-invalid');
+    });
+
+    if (!form[0].checkValidity()) {
+        form.find(":required").each(function() {
+            if (!this.checkValidity()) {
+                $(this).addClass("is-invalid");
+            }
+        });
+        return;
+    }
+
+    const formData = new FormData(form[0]);
+
+    $.ajax({
+        type: method,
+        dataType: 'json',
+        data: formData,
+        contentType: false,
+        processData: false,
+        url: endpoint,
+        success: function(response) {
+            if (!response.status || response.status === "success") {
+                form[0].reset();
+                closeModal();
+                openModal(successModal);
+                return;
+            }
+            
+            if (errorModal.length) {
+                closeModal();
+                openModal(errorModal);
+                return;
+            } else {
+                alert('Error: ' + (response.message || 'Unknown error'));
+                return;
+            }
+        },
+        error: function(error) {
+            if (errorModal.length) {
+                closeModal();
+                openModal(errorModal);
+                return;
+            } else {
+                alert('Error: ' + error);
+                return;
+            }
+        }
+    });
+}
