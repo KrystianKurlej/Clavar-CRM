ecada9c0f6abd0a0980a0b65e28c08b3f691ec07
Krystian Kurlej <235661@student.uek.krakow.pl>
2025-08-10 15:34:21 +0200

    Uproszczono funkcję logoutSubmit poprzez przeniesienie jej z widoku do pliku auth.js oraz usunięcie zbędnego skryptu z szablonu nawigacji.

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,6 +1,6 @@
 async function loginSubmit(form){
     const data = Object.fromEntries(new FormData(form));
-    
+
     try {
         const res = await fetch(form.action, { method:'POST', body: JSON.stringify(data), headers: { 'Content-Type': 'application/json' }, credentials:'include' });
         const json = await res.json();
@@ -8,5 +8,15 @@ async function loginSubmit(form){
     } catch(e){}
 
     location.reload();
+    return false;
+}
+
+async function logoutSubmit(form){
+    try {
+      const data = new FormData(form);
+      await fetch(form.action, { method:'POST', body: JSON.stringify(Object.fromEntries(data)), headers: { 'Content-Type': 'application/json' }, credentials:'include' });
+    } catch(e){}
+    
+    location.href = '/login';
     return false;
 }
